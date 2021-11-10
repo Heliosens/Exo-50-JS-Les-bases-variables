74b7951b54d64a01dbb067b0ff7bcbe50e9f8253
Sylvie-B <unsolo@hotmail.fr>
2021-11-10 11:29:54 +0100

    exo 3 prompt + alert

---
+++ exo 2/app2.js
@@ -0,0 +1,10 @@
+let nom = "Doe";
+let prenom = "John";
+let ville = "New-York";
+
+alert(
+    "Nom : " + nom +
+    "\nPrénom : " + prenom +
+    "\nville : " + ville
+)
+
---
+++ exo 3/app3.js
@@ -0,0 +1,3 @@
+let nom = prompt( "Entrez votre prénom");
+
+alert("Bonjour, " + nom);
