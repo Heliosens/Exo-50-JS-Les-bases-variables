0a8be4c902f0a00b8cdd4ae50f1030bcf5813832
Sylvie-B <unsolo@hotmail.fr>
2021-11-10 11:35:12 +0100

    exo 4 : 3 prompt et alert

---
+++ exo 4/app4.js
@@ -0,0 +1,9 @@
+let nom = prompt("Entrez le nom");
+let prenom = prompt("Entrez le prénom");
+let ville = prompt("Entrez la ville");
+
+alert(
+    "Nom : " + nom +
+    "\nPrénom : " + prenom +
+    "\nville : " + ville
+)
